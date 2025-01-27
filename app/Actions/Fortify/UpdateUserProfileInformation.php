e6acd20d291e4e5f22ca12d2cb34f7ac5d850752
Adrian-FordelPlus <alu@fordelplus.dk>
2025-01-27 12:13:41 +0100

    Added phone nr and employee id on update user profile

--- app/Actions/Fortify/UpdateUserProfileInformation.php
+++ app/Actions/Fortify/UpdateUserProfileInformation.php
@@ -20,6 +20,8 @@ class UpdateUserProfileInformation implements UpdatesUserProfileInformation
         Validator::make($input, [
             'name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'email', 'max:255', Rule::unique('users')->ignore($user->id)],
+            'phone_nr' => ['nullable', 'string', 'max:15'],
+            'employee_id ' => ['nullable', 'string', 'max:50'],
             'photo' => ['nullable', 'mimes:jpg,jpeg,png', 'max:1024'],
         ])->validateWithBag('updateProfileInformation');
 
@@ -35,6 +37,8 @@ class UpdateUserProfileInformation implements UpdatesUserProfileInformation
             $user->forceFill([
                 'name' => $input['name'],
                 'email' => $input['email'],
+                'phone_nr' => $input['phone_nr'] ?? $user->phone_nr, 
+                'employee_id' => $input['employee_id'] ?? $user->employee_id, 
             ])->save();
         }
     }
@@ -49,6 +53,8 @@ class UpdateUserProfileInformation implements UpdatesUserProfileInformation
         $user->forceFill([
             'name' => $input['name'],
             'email' => $input['email'],
+            'phone_nr' => $input['phone_nr'] ?? $user->phone_nr, 
+            'employee_id' => $input['employee_id'] ?? $user->employee_id, 
             'email_verified_at' => null,
         ])->save();
 
